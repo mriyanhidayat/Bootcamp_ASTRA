2b2a9d05623cee3c5f66b095420200d3079239a5
Riyanmh <riyanmh17@gmail.com>
2022-03-14 16:29:32 +0700

    initial project

---
+++ Fundamental/day01/ConsoleIntro.cs
@@ -0,0 +1,26 @@
+﻿namespace day01
+{
+    class ConsoleIntro
+    {
+        public static void ShowMultiConsole()
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write(" Code id bootcamp ");
+
+            //02. concat
+            Console.WriteLine("Code Academy" + " Bootcamp.Net");
+
+            Console.WriteLine("Code Academy \nC");
+
+            //04 quote
+            Console.WriteLine("I love ProgramingC \"C\"");
+            //05 verbatim 
+            Console.WriteLine(@"I love u Programing
+with Bootcamp 
+Code Id");
+            //formating number parameter
+            Console.WriteLine("Tanggal {0} Suhu {1}", DateTime.Now, 73.2);
+        }
+    }
+}
